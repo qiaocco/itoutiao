8db67e824a08c12334342a49cdbfdd64cada402e
jason <jasonqiao36@gmail.com>
2019-05-24 15:19:43 +0800

    将配置文件拆分成多个

--- itoutiao/settings/base.py
+++ itoutiao/settings/base.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'user',
+    'core',
 
     'django.contrib.admin',
     'django.contrib.auth',
--- itoutiao/settings/develop.py
+++ itoutiao/settings/develop.py
@@ -1,3 +1,20 @@
 from .base import *
 
 DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'itoutiao',
+        "USER": os.getenv("MYSQL_USER"),
+        "PASSWORD": os.getenv("MYSQL_PASSWORD"),
+        "HOST": os.getenv("MYSQL_HOST"),
+        "PORT": os.getenv("MYSQL_PORT"),
+        "CONN_MAX_AGE": 60,
+        "OPTIONS": {
+            "autocommit": True,
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            "charset": "utf8mb4",
+        },
+    }
+}
