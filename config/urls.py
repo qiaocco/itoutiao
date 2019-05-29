714014b922e1129cff68cae9c7bc691f7bfdc73f
jason <jasonqiao36@gmail.com>
2019-05-29 15:20:41 +0800

    修复wsgi获取不到环境变量的问题

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 
-from itoutiao.core import views
+from core import views
+from core.views import PostViewSet
 
-urlpatterns = [path("", views.index, name="index"), path("admin/", admin.site.urls)]
+router = routers.DefaultRouter()
+router.register("post", PostViewSet, base_name="post")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("index", views.index, name="index"),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+]
--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+profile = os.environ.get("ITOUTIAO_PROFILE", "develop")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"config.settings.{profile}")
 
 application = get_wsgi_application()
